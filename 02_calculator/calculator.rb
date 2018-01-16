df9ea84e5599d722c11aeccbe73e6d35e3395c4f
Segolene Alquier <segolene.alquier@yahoo.com>
2018-01-16 17:28:18 +0100

    calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,31 @@
-#write your code here
+def add(a, b)
+  add = a + b
+end
+
+def subtract(c, d)
+  subtract = c - d
+end
+
+def sum(array)
+  j = 0
+  array.each do |i|
+    j += i
+  end
+  return j
+end
+
+def multiply(*num)
+  num.inject(:*)
+end
+
+def power(e,f)
+  power = e**f
+end
+
+def factorial(g)
+  h = 1
+  g.times do |num|
+    h *= (num+1)
+  end
+  return h
+end
